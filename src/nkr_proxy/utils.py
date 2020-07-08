3cc7d4c79386b3c8c0a2167c554cd98ec0d24101
Emilia Lipponen <emilia.lipponen@csc.fi>
2020-07-08 13:09:09 +0300

    Change the order of parameters

--- src/nkr_proxy/utils.py
+++ src/nkr_proxy/utils.py
@@ -12,7 +12,7 @@ from nkr_proxy.settings import settings
 logger = logging.getLogger(__name__)
 
 
-def http_request(*args, method='get', **kwargs):
+def http_request(method='get', *args, **kwargs):
     if settings.DEBUG:
         logger.debug('HTTP request begin with data:')
         #logger.debug('args: %r' % args)
